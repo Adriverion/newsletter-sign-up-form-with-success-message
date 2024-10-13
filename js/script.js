2d2a82f81b16ed4475d566b9741e29f82a798c4d
Adriverion <dantejose7@gmail.com>
2024-10-13 14:38:48 -0400

    script restructuring for better error control

--- js/script.js
+++ js/script.js
@@ -2,27 +2,45 @@ const form = document.getElementById('form');
 const formInput = document.getElementById('form-email');
 const errorLabel = document.getElementById('form-error-label');
 const confirmMessageButton = document.getElementById('form-message-btn');
+const messageEmail = document.getElementById('message-email');
 const containers = document.getElementsByClassName('box');
+
+const inputStateChange = (messageError = null) => {
+    if (messageError == null) {
+        formInput.classList.remove("form__input--error");
+        formInput.placeholder = "email@company.com";
+        errorLabel.textContent = "";
+    } else {
+        formInput.classList.add("form__input--error");
+        formInput.placeholder = "ash#loremcompany.com";
+        errorLabel.textContent = messageError;
+    }
+
+    formInput.value = "";
+}
+
+const validateInput = value => {
+    const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    
+    if (value == "" || value == undefined) inputStateChange("Missing value");
+    else if(!pattern.test(value)) inputStateChange("Valid email required");
+    else {
+        inputStateChange();
+        messageEmail.textContent = value;
+        return true;
+    }
+
+    return false;
+}
 
 const switchViews = () => {
     for (const box of containers) box.classList.toggle("box--hidden");
 }
 
-formInput.addEventListener('input', () => {
-    if (formInput.validity.valid) {
-        formInput.classList.remove("form__input--error");
-        errorLabel.textContent = "";
-    } else {
-        formInput.classList.add("form__input--error");
-        errorLabel.textContent = "valid email required";
-    }
-});
+confirmMessageButton.addEventListener('click', switchViews);
 
 form.addEventListener('submit', event => {
     event.preventDefault();
-    switchViews();
+    if (validateInput(formInput.value)) switchViews();
 });
 
-confirmMessageButton.addEventListener('click', event => {
-    switchViews();  
-})
